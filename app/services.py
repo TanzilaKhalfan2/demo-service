ad3dfeddab4a8d0215cc0f39c64e362a61179aae
Tanzila <tanzilakhalfan2@gmail.com>
2024-10-24 21:23:24 +0200

    Resolve: lint issues

--- app/services.py
+++ app/services.py
@@ -1,7 +1,9 @@
 import requests
 from app.utils import log_error
 
+
 SWAPI_URL = "https://swapi.dev/api/people/"
+
 
 def fetch_and_sort_people():
     try:
@@ -21,6 +23,7 @@ def fetch_and_sort_people():
     except requests.exceptions.RequestException as e:
         log_error(e)
         raise RuntimeError("Failed to fetch data from SWAPI")
+
 
 def fetch_person(person_id):
     try:
