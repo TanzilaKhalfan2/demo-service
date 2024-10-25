344aab120d92286595d0197e742b57c9079c6bf5
Tanzila <tanzilakhalfan2@gmail.com>
2024-10-25 20:46:58 +0200

    Refactor run.sh: Update Helm chart repository and search for versions

--- run.sh
+++ run.sh
@@ -7,13 +7,15 @@ kubectl cluster-info --context kind-bmw-demo
 # Load the Docker image into the Kind cluster
 kind load docker-image bmw-microservice:latest --name bmw-demo
 # Add the Helm chart repository
-helm repo add bmwCharts https://tanzilakhalfan2.github.io/demo-bmw-microservice/
+# helm repo add bmwCharts https://tanzilakhalfan2.github.io/demo-bmw-microservice/
 # Update the Helm chart repository
 helm repo update
 # List the Helm chart repositories
 helm repo list
 # Search for the Helm chart
 helm search repo bmwCharts
+# Search for the Helm chart versions
+helm search repo bmwCharts/demo-bmw-microservice --versions
 # Show the Helm chart
 helm show chart bmwCharts/demo-bmw-microservice
 # Install Helm chart to Kind cluster
